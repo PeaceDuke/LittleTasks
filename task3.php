113feb2fc69b991120b1da69956e1ebd05d6a32d
PeaceDuke <32223423+PeaceDuke@users.noreply.github.com>
2018-07-23 22:49:20 +0500

    Update task3.php

--- task3.php
+++ task3.php
@@ -1,8 +1,12 @@
 <?php
 
+#поток ввода
 $input = trim(fgets(STDIN));
+#делим на массив строк
 $arr = explode(" ", $input);
+#считаем колличество включений каждого элемента
 $arr = array_count_values($arr);
+#выбираем наибольшее и выводим
 echo max($count);
 
 ?>
